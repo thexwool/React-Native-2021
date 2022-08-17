a932e972f65234f3a970d1abee2e05e24108e010
thexwool <allan.dsm95@gmail.com>
2022-08-17 18:06:56 -0300

    commit

--- hello.ts
+++
@@ -1,3 +0,0 @@
-let olamundo = 'Hello World'
-
-console.log(olamundo)
---
+++ myskills/src/components/Button.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {
+    TouchableOpacity,
+    Text,
+    StyleSheet
+} from 'react-native'
+
+export default function Button() {
+    return (
+        <TouchableOpacity
+            style={styles.button}
+            activeOpacity={0.7}
+        //onPress={handleNewAddSkill}
+        >
+            <Text style={styles.buttonText}>
+                Add
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#A370F7',
+        padding: 15,
+        borderRadius: 7,
+        alignItems: 'center',
+        marginTop: 20
+    },
+    buttonText: {
+        color: '#FFF',
+        fontSize: 17,
+        fontWeight: 'bold'
+    }
+})
